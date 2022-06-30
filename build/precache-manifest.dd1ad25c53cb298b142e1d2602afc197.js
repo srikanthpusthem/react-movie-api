0a1dbe2110dd5ace2345428eaef8917fd26c61da
srikanthpusthem <srikanthpusthem@gmail.com>
2022-06-30 08:02:29 +0530

    my first commit

---
+++ build/precache-manifest.dd1ad25c53cb298b142e1d2602afc197.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7949baf25495aa365aa0186ab8b02e",
+    "url": "/react-movie-api/index.html"
+  },
+  {
+    "revision": "ac6b1a38ec081c17ad3f",
+    "url": "/react-movie-api/static/css/main.b98dbe52.chunk.css"
+  },
+  {
+    "revision": "9be2568482fc1b1ae314",
+    "url": "/react-movie-api/static/js/2.6426c65b.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/react-movie-api/static/js/2.6426c65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac6b1a38ec081c17ad3f",
+    "url": "/react-movie-api/static/js/main.a6babb10.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/react-movie-api/static/js/main.a6babb10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "224a6e9e04cb715b885c",
+    "url": "/react-movie-api/static/js/runtime-main.3b9032dc.js"
+  }
+]);
